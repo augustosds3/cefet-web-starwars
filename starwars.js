b8f6fafe550fccf6c8c944ccc03d032a41bb36a6
augustosd3 <augustosd3@gmail.com>
2017-04-25 12:17:30 -0300

    MOD

--- starwars.js
+++ starwars.js
@@ -3,3 +3,23 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+	$.ajax({
+		url:'http://swapi.co/api/films/',
+		method: 'GET',
+
+		success: function(resposta){
+			console.dir(resposta);
+			let i = 0;
+			$("ul").empty();
+
+			let results = resposta.results;
+			for(let res of results){
+				$("ul").append('<li>'+res.title+ '</li>');
+				$("ul").attr('url', res.url);
+				console.log(res.url);
+
+			}
+		}
+
+	})
+
